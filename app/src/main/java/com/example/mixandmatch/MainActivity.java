2f726bd5ebc6e68aacf1d8eba2bddf674028c5e4
Kai Yaneza <92856337+yaneza-teamlight@users.noreply.github.com>
2023-03-15 21:08:01 +0800

    - more layout

--- app/src/main/java/com/example/mixandmatch/MainActivity.java
+++ app/src/main/java/com/example/mixandmatch/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.mixandmatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
     DBHandler myDB;
@@ -13,17 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDB = new DBHandler(this);
+
     }
 
+    public void click_info(View view) {
+        Intent intent = new Intent(this, information.class);
+        startActivity(intent);
+    }
 
-    public void click_settings(View view) {
+    public void click_leaderboard(View view) {
         Intent intent = new Intent(this, settings.class);
         startActivity(intent);
     }
 
-    public void click_info(View view) {
-    }
-
-    public void click_leaderboard(View view) {
+    public void click_delete(View view) {
     }
 }
---
+++ app/src/main/java/com/example/mixandmatch/game.java
@@ -0,0 +1,45 @@
+package com.example.mixandmatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+    //randomize cards
+    //3x2 4x2 5x2 6x2
+//    private List<Integer> generateCardPlacement(String difficulty){
+//        Integer[] cards = {1, 1, 2, 2};
+//        if (Objects.equals(difficulty, "easy")){
+//            //3x2
+//            cards.addAll(new Integer[]{3, 3});
+//        } else if (Objects.equals(difficulty, "moderate")) {
+//            //4x2
+//            cards.addAll(new Integer[]{3, 3, 4, 4});
+//            Integer[] cards = {1, 1, 2, 2, 3, 3, 4, 4};
+//        } else if (Objects.equals(difficulty, "difficult")) {
+//            //4x2
+//            Integer[] cards = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+//        } else {
+//            Integer[] cards = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6};
+//        }
+//        List<Integer> intList = Arrays.asList(cards);
+//        Collections.shuffle(intList);
+//        intList.toArray(cards);
+//        return ;
+//    }
+
+
+}
