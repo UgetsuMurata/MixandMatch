1262fc8e983d30047a187f7097b2ab60c8a95592
Angelo Echano <grey12angelo@gmail.com>
2023-03-23 15:44:19 +0800

    update add_user

--- app/src/main/java/com/example/mixandmatch/add_user.java
+++ app/src/main/java/com/example/mixandmatch/add_user.java
@@ -2,13 +2,33 @@ package com.example.mixandmatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class add_user extends AppCompatActivity {
+
+    DBHandler DB;
+
+    EditText myUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+
+        myUsername = findViewById(R.id.user_name);
+    }
+
+    public void submitButton(View view) {
+
+        String name = myUsername.getText().toString();
+
+        DB.insertUser(name);
+
+        Intent intent = new Intent(add_user.this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
